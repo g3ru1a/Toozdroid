1d6fecbffee271b898f798e1413b842fb3b1cea7
G3ru1a <32644617+EgidiuFarcas@users.noreply.github.com>
2020-05-13 12:33:26 +0300

    fixed request list timer

--- requests_node/tools.js
+++ requests_node/tools.js
@@ -230,7 +230,7 @@ exports.list = async function (message, args) {
 
     let filter = (reaction, user) => user.id == authorID && (reaction.emoji.name == '⬅' || reaction.emoji.name == '➡');
 
-    let collector = rep.createReactionCollector(filter, { time: 5000 });
+    let collector = rep.createReactionCollector(filter, { time: 120000 });
 
     collector.on('collect', async (reaction, user) => {
         let col = reaction;
